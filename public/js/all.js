4dffc6dbf592e74b03c2969423c38ef35b6d9bdd
wangzhe <lancocos@gmail.com>
2019-04-13 16:48:05 +0800

    init

---
+++ public/js/all.js
@@ -0,0 +1,6 @@
+function hello(){
+    console.log("### hello");
+}
+function hello2(){
+    console.log("### hello2");
+}
---
+++ resources/assets/js/components/simple-list.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+  <h1>{{title}}</h1>
+    <ul>
+        <li v-for="(item,index) in lists">{{item.id}}|{{item.title}}|{{item.time}} <button v-on:click="del(index)"  >delete</button> </li>
+    </ul>
+        <input placeholder="id" v-model="id"/>
+        <input placeholder="title" v-model="tit"/>
+        <input placeholder="time" v-model="time"/>
+        <input type="button" v-on:click="add" value="add"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "simple-list",
+        mounted:function(){
+            console.log("###");
+        },
+        data:function(){
+          return {
+              lists:[
+                  {id:1,title:'xxx1',time:'2019-12-12'},
+                  {id:2,title:'xxx2',time:'2019-12-13'},
+                  {id:3,title:'xxx3',time:'2019-12-14'},
+              ],
+              title:"title",
+              id:'',
+              tit:'',
+              time:''
+          }
+        },
+        methods:{
+            add:function(){
+               var newItem={
+                   id:this.id,
+                   title:this.tit,
+                   time:this.time,
+               };
+               this.lists.push(newItem);
+            },
+            del:function(id){
+                this.lists.splice(id,1);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
